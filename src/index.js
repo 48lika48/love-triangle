7a0aef1e5a415b39128c66f022ac2c2ac713af0d
48lika48 <48lika48@mail.ru>
2019-03-18 17:39:47 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -3,5 +3,18 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+length = [preferences.length]; 
+count = 0; 
+  for (i = 0; i<length; i++) { 
+    Spichonee1 = preferences[i] - 1; 
+    if (Spichonee1 == i) continue; 
+    Spichonee2 = preferences[Spichonee1] - 1; 
+    if (Spichonee1 == Spichonee2) continue; 
+    Spichonee3 = preferences[Spichonee2] - 1; 
+    if (Spichonee3 == Spichonee2) continue; 
+    if (Spichonee3 == i) 
+    count++; 
+  } 
+  return count / 3; 
+}
+
